73119a59947aa6d6eb00b35f7f7304260d130aeb
Math2359 <64596723+Math2359@users.noreply.github.com>
2025-02-25 00:43:48 +0000

    Testes com o Jest

---
+++ tests/math.test.js
@@ -0,0 +1,17 @@
+const math = require('../math');
+
+test('Soma de 2 + 3 = 5', () => {
+    expect(math.add(2, 3)).toBe(5)
+})
+
+test('Soma de 2 - 3 = 5', () => {
+    expect(math.subtract(2, 3)).toBe(-1)
+})
+
+test('Divisão de 6 / 3 = 2', () => {
+    expect(math.divide(6, 3)).toBe(3)
+})
+
+test('Multiplicação de 4 * 5 = 20', () => {
+    expect(math.multiply(4, 5)).toBe(20)
+})
